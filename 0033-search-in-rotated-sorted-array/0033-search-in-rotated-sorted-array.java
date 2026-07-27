e3bfeba60d239ef7db3ebcde71b616c193cb8096
Kartik Sharma <kartiksrm06@gmail.com>
2026-07-27 22:48:04 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.3%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            // Left half is sorted
-            else if (nums[lo] <= nums[mid]) {
-                if (nums[lo] <= target && target < nums[mid]) {
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[hi]) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 1;
-                }
+        return zoey(nums,target);
+    }
+
+    public static int zoey(int[] arr,int target){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==target){
+                return i;
             }
         }
         return -1;
