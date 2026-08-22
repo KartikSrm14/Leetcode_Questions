670e04a18d5454d0c81c5af3ab7ccd696b2bbb09
Kartik Sharma <kartiksrm06@gmail.com>
2026-08-22 10:04:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (66.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,14 +1,49 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return zoey(nums,target);
+        return zoey(nums, target);
     }
 
     public static int zoey(int[] arr,int target){
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==target){
-                return i;
+
+        int k = 0;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i-1]<arr[i]){
+                k++;
+            }
+            else if(arr[i-1]>arr[i]){
+                break;
             }
         }
-        return -1;
+
+        int lo = 0;
+        int hi = k;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                hi = mid-1;
+            }
+            else{
+                lo= mid+1;
+            }
+        }
+
+        int lo2 = k+1;
+        int hi2 = arr.length-1;
+        while(lo2<=hi2){
+            int mid2 = (lo2+hi2)/2;
+            if(arr[mid2] == target){
+                return mid2;
+            }
+            else if(arr[mid2] > target){
+                hi2 = mid2-1;
+            }
+            else{
+                lo2= mid2+1;
+            }
+    }
+    return -1;
     }
 }
